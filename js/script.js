03aaca9ca18b48cce7c7c2e2ee063bdfb74c42d4
manoti <52492406+manotim@users.noreply.github.com>
2025-03-02 01:19:10 +0300

    refactored

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ function toggleMenu() {
     closeIcon.classList.toggle('active');
 }
 
+ // Show button when user scrolls down
+ window.onscroll = function() {
+    console.log('from window scroll');
+    
+    const backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Smooth scroll to top
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from redirecting or reloading
+    alert("Form submitted successfully!"); 
+    window.location.href = "http://127.0.0.1:5500/home.html";
+});
